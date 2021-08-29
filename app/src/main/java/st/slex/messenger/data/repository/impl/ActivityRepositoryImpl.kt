507dccabf3d779f46fe0c1ca8756b35e20889416
slex <ilya977.077@gmail.com>
2021-08-30 00:49:50 +0300

    Remove unused code in Main repository

--- app/src/main/java/st/slex/messenger/data/repository/impl/ActivityRepositoryImpl.kt
+++ app/src/main/java/st/slex/messenger/data/repository/impl/ActivityRepositoryImpl.kt
@@ -5,10 +5,6 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.channels.awaitClose
-import kotlinx.coroutines.channels.trySendBlocking
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.withContext
 import st.slex.messenger.data.model.ContactModel
@@ -16,7 +12,6 @@ import st.slex.messenger.data.repository.interf.ActivityRepository
 import st.slex.messenger.data.service.DatabaseSnapshot
 import st.slex.messenger.utilites.*
 import st.slex.messenger.utilites.result.EventResponse
-import st.slex.messenger.utilites.result.Resource
 import javax.inject.Inject
 
 @ExperimentalCoroutinesApi
@@ -25,16 +20,6 @@ class ActivityRepositoryImpl @Inject constructor(
     private val databaseReference: DatabaseReference,
     private val auth: FirebaseAuth
 ) : ActivityRepository {
-
-    override suspend fun isAuthorise(): Flow<Resource<FirebaseAuth>> = callbackFlow {
-        val auth = FirebaseAuth.getInstance()
-        val event = if (auth.currentUser != null) {
-            trySendBlocking(Resource.Success(auth)).isSuccess
-        } else {
-            trySendBlocking(Resource.Failure(Exception("Current user is null"))).isFailure
-        }
-        awaitClose { event }
-    }
 
     override suspend fun signOut() = withContext(Dispatchers.IO) {
         auth.signOut()
--- app/src/main/java/st/slex/messenger/data/repository/interf/ActivityRepository.kt
+++ app/src/main/java/st/slex/messenger/data/repository/interf/ActivityRepository.kt
@@ -1,12 +1,8 @@
 package st.slex.messenger.data.repository.interf
 
-import com.google.firebase.auth.FirebaseAuth
-import kotlinx.coroutines.flow.Flow
 import st.slex.messenger.data.model.ContactModel
-import st.slex.messenger.utilites.result.Resource
 
 interface ActivityRepository {
-    suspend fun isAuthorise(): Flow<Resource<FirebaseAuth>>
     suspend fun signOut()
     suspend fun statusOnline()
     suspend fun statusOffline()
